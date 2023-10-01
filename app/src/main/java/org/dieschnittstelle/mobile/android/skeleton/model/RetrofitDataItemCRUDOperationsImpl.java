b252ad6d09a386d8f53aa4d59536d843cf93ca60
Friederike Buchner <s81140@bht-berlin.de>
2023-10-01 18:08:14 +0200

    Zwischenstopp: Datenzugriff mit Web API, min. 55

--- app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/model/RetrofitDataItemCRUDOperationsImpl.java
+++ app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/model/RetrofitDataItemCRUDOperationsImpl.java
@@ -2,9 +2,41 @@ package org.dieschnittstelle.mobile.android.skeleton.model;
 
 import java.util.List;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
 public class RetrofitDataItemCRUDOperationsImpl implements IToDoCRUDOperations {
 
+    public static interface ToDoResource {
 
+        @POST("/api/todos")
+        public ToDo create(@Body ToDo item);
+
+        @GET("/api/todos")
+        public List<ToDo> readAll();
+
+        @GET("/api/todos/{todoId}")
+        public ToDo read(@Path("todoId") long id);
+
+        @PUT("/api/todos/{todoId}")
+        public ToDo update(@Path("todoId") long id, @Body ToDo item);
+
+    }
+
+    private ToDoResource toDoResource;
+
+    public RetrofitDataItemCRUDOperationsImpl() {
+        Retrofit webapiBase = new Retrofit.Builder()
+        //        .baseUrl("http://10.0.2.2:8080")
+                .baseUrl("http://192l168.2.116:38605")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
 
     @Override
     public ToDo createToDo(ToDo item) {
