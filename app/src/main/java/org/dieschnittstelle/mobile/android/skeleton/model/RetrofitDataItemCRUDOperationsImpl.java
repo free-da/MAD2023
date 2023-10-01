ab4acf30dd837dba2204c0a615b6e10a3ac71dc4
Friederike Buchner <s81140@bht-berlin.de>
2023-10-01 17:40:06 +0200

    Zwischenstopp: Beginn Remote DB-Anbindung

---
+++ app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/model/RetrofitDataItemCRUDOperationsImpl.java
@@ -0,0 +1,33 @@
+package org.dieschnittstelle.mobile.android.skeleton.model;
+
+import java.util.List;
+
+public class RetrofitDataItemCRUDOperationsImpl implements IToDoCRUDOperations {
+
+
+
+    @Override
+    public ToDo createToDo(ToDo item) {
+        return null;
+    }
+
+    @Override
+    public List<ToDo> readAllToDos() {
+        return null;
+    }
+
+    @Override
+    public ToDo readToDo(long id) {
+        return null;
+    }
+
+    @Override
+    public boolean updateToDo(ToDo item) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteToDo(long id) {
+        return false;
+    }
+}
