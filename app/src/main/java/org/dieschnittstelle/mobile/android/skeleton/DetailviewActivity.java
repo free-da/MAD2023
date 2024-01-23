6d36aa5588132a53dc1a3c56eda3524efaf71f11
Friederike Buchner <s81140@bht-berlin.de>
2024-01-23 11:12:07 +0100

    Zwischenstopp: more Databinding

--- app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/DetailviewActivity.java
+++ app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/DetailviewActivity.java
@@ -20,11 +20,6 @@ public class DetailviewActivity extends AppCompatActivity {
     public static final int ITEM_CREATED = 1;
     public static final int ITEM_EDITED = 2;
 
-    private TextView itemNameView;
-    private TextView itemDescriptionView;
-
-    private FloatingActionButton fab;
-
     private ToDo item;
 
     private ActivityDetailviewBinding binding;
@@ -35,33 +30,17 @@ public class DetailviewActivity extends AppCompatActivity {
         // A) determine the view
         this.binding = DataBindingUtil.setContentView(this,R.layout.activity_detailview);
 
-        // B) prepare initialising the view by reading out its elements
-        itemNameView = findViewById(R.id.itemName);
-        itemDescriptionView = findViewById(R.id.itemDescription);
-        fab = findViewById(R.id.fab);
-
-        // C) bind data to the view element
+        // B) determine the data
         this.item = (ToDo)getIntent().getSerializableExtra(ARG_ITEM);
-        // C.1) distinguish between the two cases for using this activity: create and edit
-        if (item != null) {
-//            itemNameView.setText(item.getName());
-//            itemDescriptionView.setText(item.getDescription());
-        } else {
+        if (item == null) {
             this.item = new ToDo();
         }
 
+        // C) pass the data to the view (it will care itself how to deal with them)
         this.binding.setController(this);
-        // D) prepare the view for user interaction
-        fab.setOnClickListener(view -> {
-            onItemSaved();
-        });
     }
 
-    protected void onItemSaved() {
-        String itemName = itemNameView.getText().toString();
-        String itemDescription = itemDescriptionView.getText().toString();
-        this.item.setName(itemName);
-        this.item.setDescription(itemDescription);
+    public void onItemSaved() {
         Intent returnIntent = new Intent();
         returnIntent.putExtra(ARG_ITEM,this.item);
         setResult(this.item.getId() == 0L ? ITEM_CREATED : ITEM_EDITED,returnIntent);
